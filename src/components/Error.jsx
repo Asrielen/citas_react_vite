2e19111c69861cfb1675b6f6c2a750adcf049152
Asrielen <axselsteppen@gmail.com>
2022-08-05 12:48:50 -0400

    first commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+
+
+const Error = ({children}) => {
+  return (
+    (<div className='bg-red-800 text-white text-center
+uppercase font-bold mb-3 rounded-md p-3'>
+  <p >{children} </p> 
+  </div>)
+  )
+}
+
+export default Error
